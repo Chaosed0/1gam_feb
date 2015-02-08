c1abb65401aec12d4d39e85998728aaa65f81931
Ed Lu <chaosed0@gmail.com>
2015-02-07 21:18:55 -0500

    get rid of unused var

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -19,7 +19,6 @@ define(['crafty', 'jquery', './VoronoiTerrain',
                         
     var terrain = new VoronoiTerrain();
     Crafty.scene("Main", function () {
-        var islandRadius = Math.min(width, height);
         var island = Crafty.e("2D, Canvas, TerrainVisualizer, CameraControls, Mouse")
             .attr({x: 0, y: 0, w: width * terrainMult, h: height * terrainMult})
             .terrainvisualizer(terrain, waterPercent, groundPercent);
