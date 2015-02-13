6c9182e549efa203e5206313ec93dc862e5f28ff
Ed Lu <chaosed0@gmail.com>
2015-02-12 19:11:21 -0500

    Revamp Util, add assert

--- public/scripts/Util.js
+++ public/scripts/Util.js
@@ -9,24 +9,36 @@ define(['seedrandom'], function(seedrandom) {
         console.log(seed);
     }
 
-    var exports = {
-        setSeed: function(seed) {
-            Math.seedrandom(seed);
-        },
-        getRandom: function(min, max) {
-            if(arguments.length > 1) {
-                return Math.random() * (max - min) + min;
-            } else {
-                return Math.random() * min;
-            }
-        },
-        close: function(v1, v2, e) {
-            if(e === undefined) {
-                e = epsilon;
-            }
-            return Math.abs(v1.x - v2.x) < e && Math.abs(v1.y - v2.y) < e;
-        },
+    var Util = {};
+
+    Util.setSeed = function(seed) {
+        Math.seedrandom(seed);
     }
 
-    return exports;
+    Util.getRandom = function(min, max) {
+        if(arguments.length > 1) {
+            return Math.random() * (max - min) + min;
+        } else {
+            return Math.random() * min;
+        }
+    }
+
+    Util.close = function(v1, v2, e) {
+        if(e === undefined) {
+            e = epsilon;
+        }
+        return Math.abs(v1.x - v2.x) < e && Math.abs(v1.y - v2.y) < e;
+    }
+
+    Util.assert = function(condition, message) {
+        if (!condition) {
+            message = message || "Assertion failed";
+            if (typeof Error !== "undefined") {
+                throw new Error(message);
+            }
+            throw message; // Fallback
+        }
+    }
+
+    return Util;
 });
