89d42d8c2b8006eeb61fef505d4fa44f7216a0e6
Ed Lu <chaosed0@gmail.com>
2015-02-25 16:38:12 -0500

    Add shortcuts for actions

--- public/scripts/LocalInputs.js
+++ public/scripts/LocalInputs.js
@@ -2,9 +2,11 @@
 define(['crafty', './Util'], function(Crafty, u) {
 
     var LocalInputs = function() {
+        var self = this;
         this.actions = null;
         this.lastSelectCallback = null;
         this.selectCallback = null;
+        Crafty.bind("KeyDown", function(e) { self.actionShortcut(e) });
     }
 
     LocalInputs.prototype.setSelectCallback = function(cb) {
@@ -24,6 +26,20 @@ define(['crafty', './Util'], function(Crafty, u) {
         this.lastSelectCallback = this.selectCallback;
         if(this.selectCallback) {
             objects.vis.bind("CellSelected", this.selectCallback);
+        }
+    }
+
+    LocalInputs.prototype.actionShortcut = function(e) {
+        if(!this.actions) {
+            return;
+        }
+
+        if(e.key >= Crafty.keys['1'] && e.key <= Crafty.keys['4']) {
+            var index = e.key - Crafty.keys['1'];
+            var action = this.actions[index];
+            if(action) {
+                action.callback();
+            }
         }
     }
     
